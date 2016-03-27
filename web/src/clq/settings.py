1c1cb1b0625cb5137a7ace564097035419b8630a
Jimmy Ho <ho@jimmyho.com>
2016-03-27 20:27:47 +0100

    Prod compose config

--- web/src/clq/settings.py
+++ web/src/clq/settings.py
@@ -71,15 +71,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'clq.wsgi.application'
 
 
+import environ
+env = environ.Env()
+
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(),
 }
+
 
 
 # Password validation
